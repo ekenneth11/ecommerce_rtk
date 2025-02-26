655ebcea2b4e9a4fa3b139ca54fc62a3a1ce2554
ekenneth11 <kennethbautista1512@gmail.com>
2025-02-26 18:22:19 -0500

    added the reducers for the cart

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
 
-const CartSlice = ({
- 
+const CartSlice = createSlice({
+ name:'cart',
+ initialState,
+ reducers:{
+    addItemsToCart(state, action){
+        const existingItem = state.cartItems.find(item => item.id === action.payload.id);
+        if (existingItem){
+            existingItem.quantity += 1;
+        }else {
+            state.cartItems.push({...action.payload, quantity:1});
+        }
+    },
+    removeItemFromCart(state, action){
+        state.cartItems = state.cartItems.filter(item => item.id !== action.payload);
+    },
+    clearCart(state){
+        state.cartItems = [];
+    },
+    increaseitemQuantity(state, action){
+        const isThere = state.cartItems.find(item => item.id === action.payload);
+        if (isThere){
+            isThere.quantity += 1;
+        }
+    },
+    decreaseItemQuantity(state, action){
+        const isThere = state.cartItems.find(item => item.id === action.payload);
+        if (isThere && isThere.quantity > 1){
+            isThere.quantity -= 1;
+        }
+    }
+ }
 });
+const initialState = {
+    cartItems: [],
+ };
 
-
+export const {
+    addItemsToCart,
+    removeItemFromCart,
+    clearCart,
+    increaseitemQuantity,
+    decreaseItemQuantity
+} = CartSlice.actions;
+export default CartSlice.reducer;
